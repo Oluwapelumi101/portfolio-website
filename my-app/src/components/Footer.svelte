2d05b6bbac1128ab89ae0c118990c10f4b0e969e
Oluwapelumi101 <50147378+Oluwapelumi101@users.noreply.github.com>
2023-01-16 09:10:43 +0100

    editting

--- my-app/src/components/Footer.svelte
+++ my-app/src/components/Footer.svelte
@@ -46,7 +46,7 @@
 
 
 <div class="footer">
-    <img class=" rounded-circle img-fluid" style="width: 4rem; height: 4rem;" src="/assets/imgs/azeez.jpeg" alt="my-avatar" />
+    <img class=" rounded-circle img-fluid" style="width: 4rem; height: 4rem;" src="/assets/imgs/azeez.jpg" alt="my-avatar" />
 
     <div class="social">
         <a href="https://github.com/Oluwapelumi101/" target="_blank" rel="noopener noreferrer">
